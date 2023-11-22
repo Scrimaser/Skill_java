7f9f194f706d2d26edc015d6d3831ecbb95dd4bd
Scrimaser <scrimaser@yandex.ru>
2023-11-22 21:18:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SkillboxWork_2_11 work_2_11 = new SkillboxWork_2_11();
+        //Проверка ингредиентов
+        work_2_11.food(1200, 500, 30, 50, 300, 80);
+        //Вывод возрастов по порядку
+        work_2_11.ageComparator(5, 34, 5);
+    }
+}
---
+++ src/SkillboxWork_2_11.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class SkillboxWork_2_11 {
+
+    public void food(int milk, int powder, int eggs, int sugar, int oil, int apple) {
+
+        if (apple >= 5) {
+            System.out.println("Яблочный сок");
+        }
+
+        if (powder >= 400 && sugar >= 10 && milk >= 1000 && oil >= 30) {
+            System.out.println("Блинчики");
+        }
+
+        if (milk >= 300 && powder >= 5 && eggs >= 5) {
+            System.out.println("Омлет");
+        }
+
+        if (apple >= 3 && milk >= 100 && powder >= 300 && eggs >= 4) {
+            System.out.println("Яблочный пирог");
+        }
+    }
+
+
+    //Получаем на вход в метод 3 возраста
+
+    public void ageComparator(int jackAge, int isoldaAge, int mickaelAge) {
+
+        //Складываем переменные возраста в массив
+        int[] agesArr = {jackAge, isoldaAge, mickaelAge};
+
+        //Сортируем массив
+        Arrays.sort(agesArr);
+
+        //Создаем переменные и задаем им значение из массива от меньшего к большему
+        var minAge = agesArr[0];
+        var middleAge = agesArr[1];
+        var maxAge = agesArr[2];
+
+        //Выводим результат
+        System.out.println(maxAge + "\n" + middleAge + "\n" + minAge);
+
+    }
+}
